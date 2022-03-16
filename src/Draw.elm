9cd8afaadbf1bfb2eea1c095dc2a815318cc8274
Rob Laine <rlainewyllie@gmail.com>
2022-03-16 18:01:41 +1100

    Implement a drawArrow function

--- src/Draw.elm
+++ src/Draw.elm
@@ -11,6 +11,26 @@ boardSquares b =
     [ ( -1, -1 ) ]
 
 
+drawArrow : Float -> Float -> Board -> String -> String -> Svg msg
+drawArrow xAnchor yAnchor b fColour bColour =
+    let
+        -- Assume the cell width is 100 units; later implement it to work with b.cellWidth
+        vertices =
+            [ ( 35.0, 0.0 )
+            , ( 65.0, 0.0 )
+            , ( 65.0, -75.0 )
+            , ( 85.0, -75.0 )
+            , ( 50.0, -90.0 )
+            , ( 35.0, -75.0 )
+            , ( 65.0, -75.0 )
+            ]
+
+        shiftedVerts =
+            shiftVerts vertices xAnchor yAnchor
+    in
+    drawPoly vertices fColour bColour []
+
+
 drawBoard : Board -> Float -> Float -> String -> String -> Svg msg
 drawBoard b angle hFact fillColour lineColour =
     svg
@@ -25,32 +45,33 @@ drawBoard b angle hFact fillColour lineColour =
         , drawCell 0 4 b angle hFact fillColour lineColour
 
         -- Second Row
-        , drawPoly (vertsForCell (0.0 * toFloat b.cellWidth + (1.0 * toFloat b.cellWidth / tan 45)) (toFloat b.cellWidth * toFloat b.dimY - (1.0 * hFact * toFloat b.cellWidth)) (toFloat b.cellWidth) angle hFact) fillColour lineColour []
-        , drawPoly (vertsForCell (1.0 * toFloat b.cellWidth + (1.0 * toFloat b.cellWidth / tan 45)) (toFloat b.cellWidth * toFloat b.dimY - (1.0 * hFact * toFloat b.cellWidth)) (toFloat b.cellWidth) angle hFact) fillColour lineColour []
-        , drawPoly (vertsForCell (2.0 * toFloat b.cellWidth + (1.0 * toFloat b.cellWidth / tan 45)) (toFloat b.cellWidth * toFloat b.dimY - (1.0 * hFact * toFloat b.cellWidth)) (toFloat b.cellWidth) angle hFact) fillColour lineColour []
-        , drawPoly (vertsForCell (3.0 * toFloat b.cellWidth + (1.0 * toFloat b.cellWidth / tan 45)) (toFloat b.cellWidth * toFloat b.dimY - (1.0 * hFact * toFloat b.cellWidth)) (toFloat b.cellWidth) angle hFact) fillColour lineColour []
-        , drawPoly (vertsForCell (4.0 * toFloat b.cellWidth + (1.0 * toFloat b.cellWidth / tan 45)) (toFloat b.cellWidth * toFloat b.dimY - (1.0 * hFact * toFloat b.cellWidth)) (toFloat b.cellWidth) angle hFact) fillColour lineColour []
+        , drawCell 1 0 b angle hFact fillColour lineColour
+        , drawCell 1 1 b angle hFact fillColour lineColour
+        , drawCell 1 2 b angle hFact fillColour lineColour
+        , drawCell 1 3 b angle hFact fillColour lineColour
+        , drawCell 1 4 b angle hFact fillColour lineColour
 
         -- Third Row
-        , drawPoly (vertsForCell (0.0 * toFloat b.cellWidth + (2.0 * toFloat b.cellWidth / tan 45)) (toFloat b.cellWidth * toFloat b.dimY - (2.0 * hFact * toFloat b.cellWidth)) (toFloat b.cellWidth) angle hFact) fillColour lineColour []
-        , drawPoly (vertsForCell (1.0 * toFloat b.cellWidth + (2.0 * toFloat b.cellWidth / tan 45)) (toFloat b.cellWidth * toFloat b.dimY - (2.0 * hFact * toFloat b.cellWidth)) (toFloat b.cellWidth) angle hFact) fillColour lineColour []
-        , drawPoly (vertsForCell (2.0 * toFloat b.cellWidth + (2.0 * toFloat b.cellWidth / tan 45)) (toFloat b.cellWidth * toFloat b.dimY - (2.0 * hFact * toFloat b.cellWidth)) (toFloat b.cellWidth) angle hFact) fillColour lineColour []
-        , drawPoly (vertsForCell (3.0 * toFloat b.cellWidth + (2.0 * toFloat b.cellWidth / tan 45)) (toFloat b.cellWidth * toFloat b.dimY - (2.0 * hFact * toFloat b.cellWidth)) (toFloat b.cellWidth) angle hFact) fillColour lineColour []
-        , drawPoly (vertsForCell (4.0 * toFloat b.cellWidth + (2.0 * toFloat b.cellWidth / tan 45)) (toFloat b.cellWidth * toFloat b.dimY - (2.0 * hFact * toFloat b.cellWidth)) (toFloat b.cellWidth) angle hFact) fillColour lineColour []
+        , drawCell 2 0 b angle hFact fillColour lineColour
+        , drawCell 2 1 b angle hFact fillColour lineColour
+        , drawCell 2 2 b angle hFact fillColour lineColour
+        , drawCell 2 3 b angle hFact fillColour lineColour
+        , drawCell 2 4 b angle hFact fillColour lineColour
 
         -- Fourth
-        , drawPoly (vertsForCell (0.0 * toFloat b.cellWidth + (3.0 * toFloat b.cellWidth / tan 45)) (toFloat b.cellWidth * toFloat b.dimY - (3.0 * hFact * toFloat b.cellWidth)) (toFloat b.cellWidth) angle hFact) fillColour lineColour []
-        , drawPoly (vertsForCell (1.0 * toFloat b.cellWidth + (3.0 * toFloat b.cellWidth / tan 45)) (toFloat b.cellWidth * toFloat b.dimY - (3.0 * hFact * toFloat b.cellWidth)) (toFloat b.cellWidth) angle hFact) fillColour lineColour []
-        , drawPoly (vertsForCell (2.0 * toFloat b.cellWidth + (3.0 * toFloat b.cellWidth / tan 45)) (toFloat b.cellWidth * toFloat b.dimY - (3.0 * hFact * toFloat b.cellWidth)) (toFloat b.cellWidth) angle hFact) fillColour lineColour []
-        , drawPoly (vertsForCell (3.0 * toFloat b.cellWidth + (3.0 * toFloat b.cellWidth / tan 45)) (toFloat b.cellWidth * toFloat b.dimY - (3.0 * hFact * toFloat b.cellWidth)) (toFloat b.cellWidth) angle hFact) fillColour lineColour []
-        , drawPoly (vertsForCell (4.0 * toFloat b.cellWidth + (3.0 * toFloat b.cellWidth / tan 45)) (toFloat b.cellWidth * toFloat b.dimY - (3.0 * hFact * toFloat b.cellWidth)) (toFloat b.cellWidth) angle hFact) fillColour lineColour []
+        , drawCell 3 0 b angle hFact fillColour lineColour
+        , drawCell 3 1 b angle hFact fillColour lineColour
+        , drawCell 3 2 b angle hFact fillColour lineColour
+        , drawCell 3 3 b angle hFact fillColour lineColour
+        , drawCell 3 4 b angle hFact fillColour lineColour
 
         -- Fifth
-        , drawPoly (vertsForCell (0.0 * toFloat b.cellWidth + (4.0 * toFloat b.cellWidth / tan 45)) (toFloat b.cellWidth * toFloat b.dimY - (4.0 * hFact * toFloat b.cellWidth)) (toFloat b.cellWidth) angle hFact) fillColour lineColour []
-        , drawPoly (vertsForCell (1.0 * toFloat b.cellWidth + (4.0 * toFloat b.cellWidth / tan 45)) (toFloat b.cellWidth * toFloat b.dimY - (4.0 * hFact * toFloat b.cellWidth)) (toFloat b.cellWidth) angle hFact) fillColour lineColour []
-        , drawPoly (vertsForCell (2.0 * toFloat b.cellWidth + (4.0 * toFloat b.cellWidth / tan 45)) (toFloat b.cellWidth * toFloat b.dimY - (4.0 * hFact * toFloat b.cellWidth)) (toFloat b.cellWidth) angle hFact) fillColour lineColour []
-        , drawPoly (vertsForCell (3.0 * toFloat b.cellWidth + (4.0 * toFloat b.cellWidth / tan 45)) (toFloat b.cellWidth * toFloat b.dimY - (4.0 * hFact * toFloat b.cellWidth)) (toFloat b.cellWidth) angle hFact) fillColour lineColour []
-        , drawPoly (vertsForCell (4.0 * toFloat b.cellWidth + (4.0 * toFloat b.cellWidth / tan 45)) (toFloat b.cellWidth * toFloat b.dimY - (4.0 * hFact * toFloat b.cellWidth)) (toFloat b.cellWidth) angle hFact) fillColour lineColour []
+        , drawCell 4 0 b angle hFact fillColour lineColour
+        , drawCell 4 1 b angle hFact fillColour lineColour
+        , drawCell 4 2 b angle hFact fillColour lineColour
+        , drawCell 4 3 b angle hFact fillColour lineColour
+        , drawCell 4 4 b angle hFact fillColour lineColour
+        , drawArrow 100 100 b "red" "red"
         ]
 
 
@@ -64,9 +85,9 @@ drawRow rowNum b angle hFact fillColour lineColour =
     drawPoly (vertsForCell (0.0 * toFloat b.cellWidth + (0.0 * toFloat b.cellWidth / tan 45)) (toFloat b.cellWidth * toFloat b.dimY - (toFloat rowNum * hFact * toFloat b.cellWidth)) (toFloat b.cellWidth) angle hFact) fillColour lineColour []
 
 
-xShiftVerts : List ( Float, Float ) -> Float -> List ( Float, Float )
-xShiftVerts x y =
-    []
+shiftVerts : List ( Float, Float ) -> Float -> Float -> List ( Float, Float )
+shiftVerts vertices xShift yShift =
+    [ ( 1.0, 1.0 ), ( 2.0, 2.0 ) ]
 
 
 drawPoly : List ( Float, Float ) -> String -> String -> List (Svg msg) -> Svg msg
